fcb1e6c16cc067cf4737f3917956ab8077df9e83
nevespgabriel <nevespgabriel@gmail.com>
2025-09-06 20:05:06 -0300

    feat: add ids and name and description properties for Meeting

--- lib/models/local.dart
+++ lib/models/local.dart
@@ -1,9 +1,11 @@
 class Local {
+  int local_id;
   String name;
   String description;
   List<double> coordinates;
 
   Local({
+    required this.local_id,
     required this.name,
     required this.description,
     required this.coordinates,
--- lib/models/meeting.dart
+++ lib/models/meeting.dart
@@ -2,9 +2,19 @@ import 'local.dart';
 import 'user.dart';
 
 class Meeting {
+  int meeting_id;
+  String name;
+  String description;
   Local local;
   List<User> users;
   DateTime datetime;
 
-  Meeting({required this.local, required this.users, required this.datetime});
+  Meeting({
+    required this.meeting_id,
+    required this.name,
+    required this.description,
+    required this.local,
+    required this.users,
+    required this.datetime,
+  });
 }
